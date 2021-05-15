ea7c6d3d2e11d47b59141ce6c41f15f5f14d6bbb
james <engrjayt200@gmail.com>
2021-05-15 23:17:13 +0100

    Implement Bar and Column chart

--- src/components/Charts/Column3D.js
+++ src/components/Charts/Column3D.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Chart from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
-const Column3D = () => {
-  return <div>chart</div>;
+// STEP 3 - Creating the JSON object to store the chart configurations
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "column3d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Most Popular",
+        yAxisName: "Stars",
+        xAxisName: "Repos",
+        xAxisNameFontSize: "16px",
+        yAxisNameFontSize: "16px",
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Column3D;
+export default ChartComponent;
